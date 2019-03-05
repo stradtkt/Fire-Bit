fa9d6e40ab71e21be6ce5499017823fa57322a30
Kevin <stradtkt22@gmail.com>
2019-03-04 20:12:46 -0500

    Initial Commit

---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef FIRE_BIT_LIBRARY_H
+#define FIRE_BIT_LIBRARY_H
+
+void hello();
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lib/include/SDL.h"
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
