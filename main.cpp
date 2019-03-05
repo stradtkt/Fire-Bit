2e7e19b96d0c2efc7e9ab8040b4c44afc529b9b3
Kevin <stradtkt22@gmail.com>
2019-03-04 20:47:18 -0500

    Added code for quit events in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 #include "include/SDL.h"
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         cout << "SDL init failed" << endl;
         return 1;
     }
-    cout << "SDL init succeeded" << endl;
+    SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SCREEN_WINDOW_SHOWN);
 
+    if(window == NULL) {
+        SDL_Quit();
+        return 2;
+    }
+    bool quit = false;
+    SDL_Event event;
+    while(!quit) {
+
+
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window)
     SDL_Quit();
     return 0;
 }
